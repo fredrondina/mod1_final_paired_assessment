51e6ab588c332f5da81e1d77173466e26003ef01
Fred Rondina <fredrondina96@gmail.com>
2020-01-12 11:33:34 -0700

    Added logscore method and test

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,15 @@ require 'pry'
 
 class Student
 
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(attribute_hash = Hash.new)
     @name = attribute_hash[:name]
     @age = attribute_hash[:age]
+    @scores = []
   end
 
+  def log_score(score)
+    @scores << score
+  end
 end
--- test/student_test.rb
+++ test/student_test.rb
@@ -12,6 +12,15 @@ class StudentTest < Minitest::Test
 
   def test_student_exists_and_has_attributes
     assert_instance_of Student, @student
-    
+    assert_equal @student.name, "Morgan"
+    assert_equal @student.age, 21
+    assert_equal @student.scores, []
+  end
+
+  def test_scores_can_be_added_to_student
+    assert_equal @student.scores, []
+    @student.log_score(89)
+    @student.log_score(78)
+    assert_equal @student.scores, [89, 78]
   end
 end
